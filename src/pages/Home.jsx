102238699232eb8fb7d777517d5b7a25ea43b297
ahmed687mohammed <ahmed673elwhady@gmail.com>
2025-09-16 21:49:13 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -84,8 +84,7 @@ export default function Home() {
               <Badge className="absolute top-2 right-2 bg-blue-600">
                 {badgeText}
               </Badge>
-            )}
-            
+            ))
           <div className="p-4">
             <h3 className="font-semibold text-lg mb-2 group-hover:text-blue-600 transition-colors line-clamp-1">
               {manga.title}
